3d331ae6ed2d794ee0d38cc90c7d14ed61dde989
ahmaduae123 <ahmadchatgptwala@gmail.com>
2025-06-02 14:45:27 +0500

    Update db.php

--- db.php
+++ db.php
@@ -1,1 +1,13 @@
-hhcugvhjcdvghdhhv
+<?php
+$host = 'localhost';
+$db = 'db5jh3gmw817mn';
+$user = 'unzm7rdkzgjny';
+$pass = 'cgudjczogdg4';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database Connection Failed: " . $e->getMessage());
+}
+?>
